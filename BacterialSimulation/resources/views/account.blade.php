23fdb0a7a89a457588c701ec6133f88f2669ed14
Unknown <tbihon@radford.edu>
2017-11-30 12:32:05 -0500

    Auto stash before merge of "master" and "origin/master"

--- BacterialSimulation/resources/views/account.blade.php
+++ BacterialSimulation/resources/views/account.blade.php
@@ -4,19 +4,16 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are at the account page!
-                </div>
-            </div>
+          <label> Account type: 
+            <input type= "text" name= "fn id="fn" /></label><br><br>
+            <label> E-mail: 
+            <input type= "text" name="em" id="em"/></lebel><br><br>
+            <lable> Total Simulation run:
+            <input type="text" name="nm" id="nm"/> </lable><br><br>
+            <lable> Account since:
+            <input type="text" name="dy" id="dy"/> </lable><br><br>  
+            <input type="button" id= "edit" value="Edit information" onclick= "validate()"/>
+            <input type="button" value="Delete Account"/>
         </div>
     </div>
 </div>
