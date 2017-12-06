e23fdd4fd6dad8e847d05ca349fe5b6e204082a7
Unknown <ccaldwell23@radford.edu>
2017-12-06 18:34:51 -0500

    Account CSS Updated

--- BacterialSimulation/resources/views/account.blade.php
+++ BacterialSimulation/resources/views/account.blade.php
@@ -4,16 +4,44 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-          <label> Account type: 
-            <input type= "text" name= "fn id="fn" /></label><br><br>
-            <label> E-mail: 
-            <input type= "text" name="em" id="em"/></lebel><br><br>
-            <lable> Total Simulation run:
-            <input type="text" name="nm" id="nm"/> </lable><br><br>
-            <lable> Account since:
-            <input type="text" name="dy" id="dy"/> </lable><br><br>  
-            <input type="button" id= "edit" value="Edit information" onclick= "validate()"/>
-            <input type="button" value="Delete Account"/>
+            <div class="panel panel-default">
+                <div class="panel-heading">Account Information: </div>
+                    <div class="panel-body">
+                        <form class="form-horizontal">
+                            <div class="form-group">
+                                <label for="account_type" class="col-md-6 control-label">Account Type: </label>
+                                <label name="account_type" id="account_type" class="control-label">Type</label>
+                            </div>
+                            <div class="form-group">
+                                <label for="email" class="col-md-6 control-label col-centered">E-mail: </label>
+                                <label name="email" id="email" class="control-label">E-mail</label>
+                            </div>
+                            <div class="form-group">
+                                <label for="total" class="col-md-6 control-label col-centered">Total Simulation run: </label>
+                                <label name="total" id="total" class="control-label">Total Simulation run</label>
+                            </div>
+                            <div class="form-group">
+                                <label for="creation" class="col-md-6 control-label col-centered">Account since: </label>
+                                <label name="creation" id="creation" class="control-label">Account since</label>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-md-8 col-centered">
+                                    <a role="button" class="btn btn-info" id='edit-link' href="/edit_account">
+                                        Edit Account
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-md-8 col-centered">
+                                    <button type="submit" class="btn btn-danger" id='delete-button'>
+                                        Delete Account
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
